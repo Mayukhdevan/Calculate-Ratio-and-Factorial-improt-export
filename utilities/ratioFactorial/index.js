7cc6466712db5df562aad5c21f0c1e7875962de8
Mayukhdevan <mayukhdevansahu@yahoo.com>
2022-12-26 08:37:23 +0000

    created a function that returns ratio and factorial of 3 numbers

---
+++ utilities/ratioFactorial/index.js
@@ -0,0 +1,11 @@
+const ratio = require("../ratio/index");
+const factorial = require("../factorial/index");
+
+const ratioAndFactorial = (num1, num2, num3) => {
+    return {
+        ratio: ratio(num1, num2),
+        factorial: factorial(num3)
+    };
+};
+
+module.exports = ratioAndFactorial;
